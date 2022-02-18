464e7e45e215e30c5007c5e63990ec1dde952a82
clebersonjose <clebersonjosea@gmail.com>
2022-02-17 21:38:36 -0300

    Home mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare, faInstagramSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-
 import Text from '../src/components/Text';
 import ArticleHeader from '../src/layouts/ArticleHeader';
 import DomainLink from '../src/infra/components/DomainLink';
@@ -20,16 +19,23 @@ const Content = styled.div`
 const Main = styled.main`
   display: grid;
   padding: 20px;
-  grid-template-columns: repeat(2, 1fr);
   height: max-content;
   align-items: center;
   max-width: 100vw;
   gap: 20px;
+
+  @media(min-width: 750px) {
+    grid-template-columns: repeat(2, 1fr);  
+  }
 `;
 
 const NavActions = styled.div`
   display: flex;
   gap: 20px;
+
+  @media(max-width: 260px) {
+    flex-direction: column;
+  }
 `;
 
 const Footer = styled.footer`
@@ -49,48 +55,48 @@ const Icon = styled(FontAwesomeIcon)`
 const Home: NextPage = () => {
   return (
     <>
-    <DomainHead>
+      <DomainHead>
         <title>Cléberson J. Andrade</title>
-    </DomainHead>
-    <Content>
-      <header></header>
-      <Main>
-        <section>
-          <ArticleHeader
-            titleChildren="Cléberson J. Andrade"
-            subtitleChildren="Programador no Grupo Cruzeiro do Sul Educacional"
-          />
+      </DomainHead>
+      <Content>
+        <header></header>
+        <Main>
+          <section>
+            <ArticleHeader
+              titleChildren="Cléberson J. Andrade"
+              subtitleChildren="Programador no Grupo Cruzeiro do Sul Educacional"
+            />
 
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar tortor convallis dolor ultricies, nec mattis lorem mattis. In eu facilisis mauris.
-          </Text>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar tortor convallis dolor ultricies, nec mattis lorem mattis. In eu facilisis mauris.
+            </Text>
 
-          <NavActions>
-            <DomainLink href="/projetos">
-              <SecondaryButton>Projetos</SecondaryButton>
-            </DomainLink>
+            <NavActions>
+              <DomainLink href="/projetos">
+                <SecondaryButton>Projetos</SecondaryButton>
+              </DomainLink>
 
-            <DomainLink href="/contato">
-              <PrimaryButton>Contato</PrimaryButton>
-            </DomainLink>
-          </NavActions>
-        </section>
-      </Main>
+              <DomainLink href="/contato">
+                <PrimaryButton>Contato</PrimaryButton>
+              </DomainLink>
+            </NavActions>
+          </section>
+        </Main>
 
-      <Footer>
-        <a href="https://github.com/clebersonjose" target='_blank' rel="noreferrer">
-          <Icon icon={faGithubSquare} />
-        </a>
+        <Footer>
+          <a href="https://github.com/clebersonjose" target='_blank' rel="noreferrer">
+            <Icon icon={faGithubSquare} />
+          </a>
 
-        <a href="https://www.linkedin.com/in/clebersonandrade/" target='_blank' rel="noreferrer">
-          <Icon icon={faLinkedin} />
-        </a>
+          <a href="https://www.linkedin.com/in/clebersonandrade/" target='_blank' rel="noreferrer">
+            <Icon icon={faLinkedin} />
+          </a>
 
-        <a href="https://www.instagram.com/cleber.png/" target='_black' rel="noreferrer">
-          <Icon icon={faInstagramSquare} />
-        </a>
-      </Footer>
-    </Content>
+          <a href="https://www.instagram.com/cleber.png/" target='_black' rel="noreferrer">
+            <Icon icon={faInstagramSquare} />
+          </a>
+        </Footer>
+      </Content>
     </>
   )
 }
