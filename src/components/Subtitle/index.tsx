db87f7e460ac6c43b910a86d27dcd873a5782239
clebersonjose <clebersonjosea@gmail.com>
2022-05-11 20:57:16 -0300

    Criando componente de subtitulo

---
+++ src/components/Subtitle/index.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Subtitle = styled.h2`
+  font-size: 2.4rem;
+  line-height: 1.25em;
+  color: ${({ theme }) => theme.darkColor};
+  font-family: ${({ theme }) => theme.fontFamily};
+
+  @media (max-width: ${({ theme }) => theme.size.tablet}) {
+    font-size: 2rem;
+  }
+
+  @media (max-width: ${({ theme }) => theme.size.mobile}) {
+    font-size: 1.8rem;
+  }
+`;
+
+export default Subtitle;
