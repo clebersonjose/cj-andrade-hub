2b64b3816ffb32ca3c5cd2f9de0e66df91dce86d
clebersonjose <clebersonjosea@gmail.com>
2022-05-11 21:03:27 -0300

    Criando pagina da home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,33 @@
 import type { NextPage } from 'next';
+import Button from '../src/components/Button';
+import Text from '../src/components/Text';
+import Footer from '../src/layouts/Footer';
+import TitleWithSubtitle from '../src/layouts/TitleWithSubtitle';
+import { HomeContent, HomeNavActions, Page } from '../styles/home';
 
 const Home: NextPage = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
+  <Page>
+    <HomeContent>
+      <TitleWithSubtitle
+        title="Cléberson J. Andrade"
+        subtitle="Programador no Grupo Cruzeiro do Sul Educacional"
+      />
+
+      <Text>
+        Programador Web a 4 anos, atualmente atuando no Grupo Cruzeiro do
+        Sul Educacional, trabalhando no momento em projetos ReactJS,
+        NodeJS (c/ Express) e WordPress (PHP).
+      </Text>
+
+      <HomeNavActions>
+        <Button type="Outline">Projetos</Button>
+
+        <Button>Contato</Button>
+      </HomeNavActions>
+    </HomeContent>
+
+    <Footer />
+  </Page>
 );
 
 export default Home;
