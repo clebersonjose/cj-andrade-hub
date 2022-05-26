8e71853a145bc84584057feefee964cc9248a938
clebersonjose <clebersonjosea@gmail.com>
2022-05-25 21:43:19 -0300

    Criando pagina de projetos

---
+++ pages/projetos/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next';
+import Footer from '../../src/layouts/Footer';
+import Header from '../../src/layouts/Header';
+import TitleWithSubtitle from '../../src/layouts/TitleWithSubtitle';
+import { ProjetosContent } from '../../styles/projetos';
+
+const Projetos: NextPage = () => (
+  <ProjetosContent>
+    <Header />
+    <TitleWithSubtitle title="Projetos" subtitle="Sem projetos no momentos" />
+    <Footer />
+  </ProjetosContent>
+);
+
+export default Projetos;
