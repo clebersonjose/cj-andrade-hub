a5574ef96a7fdc5a8697cd01e878f9c716ca6b78
clebersonjose <clebersonjosea@gmail.com>
2022-05-25 21:40:41 -0300

    Separando logica do estilo do componente titulo com sub titulo

--- src/layouts/TitleWithSubtitle/index.tsx
+++ src/layouts/TitleWithSubtitle/index.tsx
@@ -1,19 +1,11 @@
-import styled from 'styled-components';
 import Subtitle from '../../components/Subtitle';
 import Title from '../../components/Title';
-import { TitleWithSubtitleProps } from './TitleWithSubtitle.interfaces';
-
-const TitleWithSubtitleContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 100%;
-`;
+import { TitleWithSubtitleProps } from './interfaces';
+import TitleWithSubtitleContainer from './styles';
 
 const TitleWithSubtitle = ({ title, subtitle }: TitleWithSubtitleProps) => (
   <TitleWithSubtitleContainer>
     <Title>{title}</Title>
-
     <Subtitle>{subtitle}</Subtitle>
   </TitleWithSubtitleContainer>
 );
---
+++ src/layouts/TitleWithSubtitle/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const TitleWithSubtitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+`;
+
+export default TitleWithSubtitleContainer;
